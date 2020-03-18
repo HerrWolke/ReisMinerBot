d4636ebfb09303016bf626444649ff13620593d8
Mr Cloud <49591248+MrCloud0@users.noreply.github.com>
2020-03-18 12:36:11 +0100

    Revert "Revert "0.5""
    This reverts commit 92b1e344ac277d48e2af4aeb33ee0614420024d2.

--- src/de/mrcloud/listeners/CommandListener.java
+++ src/de/mrcloud/listeners/CommandListener.java
@@ -11,11 +11,14 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 import java.io.*;
+import java.lang.reflect.Array;
 
 public class CommandListener extends ListenerAdapter {
     int delCheckerNumber = 0;
     int Test = 0;
-    String read = "";
+    public String read = "";
+    public long messageID;
+    static Message msg;
 
     @Override
     public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent e) {
@@ -30,120 +33,105 @@ public class CommandListener extends ListenerAdapter {
         String[] splitMsg = rawMsg.split(pattern);
         super.onGuildMessageReceived(e);
         if (splitMsg[0].equalsIgnoreCase("&Config")) {
-
-
-//            //            if (delCheckerNumber > 0) {
-////                MessageHistory msgHistory = new MessageHistory(TxtChannel);
-////                List<Message> toDelMsgs;
-////
-////                toDelMsgs = msgHistory.retrievePast(delCheckerNumber + 1).complete();
-////                delCheckerNumber = 0;
-////                TxtChannel.deleteMessages(toDelMsgs).queue();
-//
-//            }
-            System.out.println(splitMsg[1]);
-
-
             //Überprüft dass eine Config angegeben wurde
             if (!splitMsg[1].isEmpty()) {
-
+                getFilesByName(splitMsg[1],TxtChannel);
                 System.out.println("User " + e.getAuthor().getName() + " hat die Datei " + splitMsg[1] + " angefordert.");
                 //Schau ob eine Datei mit dem 1.Argument + .cfg existiert
-                if (new File(splitMsg[1] + ".cfg").exists()) {
-                    File config = new File(splitMsg[1] + ".cfg");
-                    TxtChannel.sendFile(config).queue();
-                } else if (new File(splitMsg[1] + ".lua").exists()) {
-                    File config = new File(splitMsg[1] + ".lua");
-                    TxtChannel.sendFile(config).queue();
-                } else {
 
-                    TxtChannel.sendMessage("This File does not exist!").queue();
-                    message.getTextChannel().getLatestMessageId();
-                }
             }
         } else if (splitMsg[0].equalsIgnoreCase("&Configs")) {
-            File files = new File("../Files.txt");
 
-            System.out.println("User " + e.getAuthor().getName() + " requested the all Configs List!");
+                File files = new File("../Files.txt");
 
-
-            String line = null;
-            try (FileWriter FileWriterName = new FileWriter(files, true);
-                 BufferedWriter Writer = new BufferedWriter(FileWriterName)) {
+                System.out.println("User " + e.getAuthor().getName() + " requested the all Configs List!");
 
 
-                if (files.exists()) {
-                    FileReader fileReader = null;
-                    try {
-                        fileReader = new FileReader(files);
-                    } catch (FileNotFoundException ex) {
-                        ex.printStackTrace();
+                String line = null;
+                try (FileWriter FileWriterName = new FileWriter(files, true);
+                     BufferedWriter Writer = new BufferedWriter(FileWriterName)) {
+
+
+                    if (files.exists()) {
+                        FileReader fileReader = null;
+                        try {
+                            fileReader = new FileReader(files);
+                        } catch (FileNotFoundException ex) {
+                            ex.printStackTrace();
+                        }
+
+
+                        BufferedReader bufferedReader = new BufferedReader(fileReader);
+                        while ((line = bufferedReader.readLine()) != null) {
+
+                            read += " " + line;
+                            delCheckerNumber++;
+
+                        }
+
+
+                        try {
+                            bufferedReader.close();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+
+                TxtChannel.sendMessage(read.replaceAll("\\s+", "\n")).queue(message1 -> {
+                    System.out.println("Es werden " + delCheckerNumber + " Emojis hinzugefügt");
+                    setMsg(message1);
+
+
+                    while (Test < delCheckerNumber && Test < 4) {
+                        message1.addReaction("U+1f1e" + (6 + Test)).queue();
+                        Test++;
+
+                    }
+
+                    if (Test != delCheckerNumber) {
+                        message1.addReaction("U+1f1ea").queue();
+                        Test++;
+                    }
+                    if (Test != delCheckerNumber) {
+                        message1.addReaction("U+1f1eb").queue();
+                        Test++;
+                    }
+                    if (Test != delCheckerNumber) {
+                        message1.addReaction("U+1f1ec").queue();
+                        Test++;
+                    }
+                    if (Test != delCheckerNumber) {
+                        message1.addReaction("U+1f1ed").queue();
+                        Test++;
+                    }
+                    if (Test != delCheckerNumber) {
+                        message1.addReaction("U+1f1ee").queue();
+                        Test++;
+                    }
+                    if (Test != delCheckerNumber) {
+                        message1.addReaction("U+1f1ef").queue();
+                        Test++;
                     }
 
 
-                    BufferedReader bufferedReader = new BufferedReader(fileReader);
-                    while ((line = bufferedReader.readLine()) != null) {
-
-                        read += " " + line;
-                        delCheckerNumber++;
+                    int counter = 0;
+                    while (Test < delCheckerNumber && Test < 20) {
+                        message1.addReaction("U+1f1f" + counter).queue();
+                        counter++;
+                        Test++;
 
                     }
 
 
-                    try {
-                        bufferedReader.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
 
-                }
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-
-
-            TxtChannel.sendMessage(read.replaceAll("\\s+", "\n")).queue(message1 -> {
-                System.out.println(delCheckerNumber);
-
-                while (Test < delCheckerNumber && Test < 4) {
-                    message1.addReaction("U+1f1e" + (6 + Test)).queue();
-                    Test++;
-                }
-
-                if (Test != delCheckerNumber) {
-                    message1.addReaction("U+1f1ea").queue();
-                    Test++;
-                }
-                if (Test != delCheckerNumber) {
-                    message1.addReaction("U+1f1eb").queue();
-                    Test++;
-                }
-                if (Test != delCheckerNumber) {
-                    message1.addReaction("U+1f1ec").queue();
-                    Test++;
-                }
-                if (Test != delCheckerNumber) {
-                    message1.addReaction("U+1f1ed").queue();
-                    Test++;
-                }
-                if (Test != delCheckerNumber) {
-                    message1.addReaction("U+1f1ee").queue();
-                    Test++;
-                }
-                if (Test != delCheckerNumber) {
-                    message1.addReaction("U+1f1ef").queue();
-                    Test++;
-                }
-
-                int counter = 0;
-                while (Test < delCheckerNumber && Test < 20) {
-                    message.addReaction("U+1f1f" + counter).queue();
-                    counter++;
-                    Test++;
-                }
-
-            });
-
+                    checker();
+                    read = "";
+                });
 
 
         } else if (splitMsg[0].equalsIgnoreCase("&Stop")) {
@@ -158,5 +146,36 @@ public class CommandListener extends ListenerAdapter {
             }
         }
 
+    } public void getFilesByName(String splitMsg, TextChannel TxtChannel) {
+        if (new File(splitMsg + ".cfg").exists()) {
+            File config = new File(splitMsg + ".cfg");
+            TxtChannel.sendFile(config).queue();
+        } else if (new File(splitMsg + ".lua").exists()) {
+            File config = new File(splitMsg + ".lua");
+            TxtChannel.sendFile(config).queue();
+        } else if(new File(splitMsg).exists()){
+            File config = new File(splitMsg);
+            TxtChannel.sendFile(config).queue();
+
+        } else {
+            TxtChannel.sendMessage("This File does not exist!").queue();
+        }
+    }
+
+    public String getRead() {
+        return read;
+    }
+
+    public static void setMsg(Message msg) {
+        CommandListener.msg = msg;
+    }
+
+    public static Message getMsg() {
+        return msg;
+    } public void checker() {
+        if(delCheckerNumber == Test) {
+            delCheckerNumber = 0;
+            Test = 0;
+        }
     }
 }
